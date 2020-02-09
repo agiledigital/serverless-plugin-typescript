dd200e25b75850efa09cb566386d29dd177768af
Sean Dawson <spdawson@agiledigital.com.au>
2020-02-09 19:24:49 +1100

    Temporarily commit dist for testing purposes

---
+++ dist/tests/index.functions.test.js
@@ -0,0 +1,93 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const src_1 = require("../src");
+const createInstance = (functions, globalRuntime) => ({
+    cli: {
+        log: jest.fn()
+    },
+    config: {
+        servicePath: 'servicePath'
+    },
+    service: {
+        provider: {
+            name: 'aws',
+            runtime: globalRuntime
+        },
+        package: {
+            individually: true,
+            include: [],
+            exclude: []
+        },
+        functions,
+        getAllFunctions: jest.fn()
+    },
+    pluginManager: {
+        spawn: jest.fn()
+    }
+});
+describe('functions', () => {
+    const functions = {
+        hello: {
+            handler: 'tests/assets/hello.handler',
+            package: {
+                include: [],
+                exclude: []
+            }
+        },
+        world: {
+            handler: 'tests/assets/world.handler',
+            runtime: 'nodejs12.x',
+            package: {
+                include: [],
+                exclude: []
+            },
+        },
+        js: {
+            handler: 'tests/assets/jsfile.create',
+            package: {
+                include: [],
+                exclude: []
+            }
+        },
+        notActuallyTypescript: {
+            handler: 'tests/assets/jsfile.create',
+            package: {
+                include: [],
+                exclude: []
+            },
+            runtime: 'go1.x'
+        },
+    };
+    describe('when the provider runtime is Node', () => {
+        it('can get filter out non node based functions', () => {
+            const slsInstance = createInstance(functions, 'nodejs10.x');
+            const plugin = new src_1.default(slsInstance, {});
+            expect(Object.values(plugin.functions).map(fn => fn.handler)).toEqual([
+                'tests/assets/hello.handler',
+                'tests/assets/world.handler',
+                'tests/assets/jsfile.create',
+            ]);
+        });
+    });
+    describe('when the provider runtime is not Node', () => {
+        it('can get filter out non node based functions', () => {
+            const slsInstance = createInstance(functions, 'python2.7');
+            const plugin = new src_1.default(slsInstance, {});
+            expect(Object.values(plugin.functions).map(fn => fn.handler)).toEqual([
+                'tests/assets/world.handler',
+            ]);
+        });
+    });
+    describe('when the provider runtime is undefined', () => {
+        it('can get filter out non node based functions', () => {
+            const slsInstance = createInstance(functions);
+            const plugin = new src_1.default(slsInstance, {});
+            expect(Object.values(plugin.functions).map(fn => fn.handler)).toEqual([
+                'tests/assets/hello.handler',
+                'tests/assets/world.handler',
+                'tests/assets/jsfile.create',
+            ]);
+        });
+    });
+});
+//# sourceMappingURL=index.functions.test.js.map
